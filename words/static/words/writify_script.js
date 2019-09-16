1aeb0a57cd6958914c71b736e2060041355f6908
Bartu Sivri <sbartu@umich.edu>
2019-09-16 10:12:52 -0400

    working on words api

--- words/static/words/writify_script.js
+++ words/static/words/writify_script.js
@@ -4,30 +4,54 @@ var resultView = new Vue({
   data: {
     json_urban: [],
     urban_display: [],
+    json_words: [],
+    words_display: [],
   },
   methods: {
-    search_urban_dic (e) {
+    search(e) {
       if(e.keyCode === 13) {
-        const query = e.target.value;
-        const url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define";
-        const headers = {
-          "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
-          "x-rapidapi-key": "d42b00b19fmsh1d8c8774483287cp1a9412jsn58fb2ac82287",
-        }
-        const params = {
-          term: query
-        }
-        axios
-          .get(url, {params, headers})
-          .then(response => {
-            this.json_urban = response.data.list;
-            this.urban_display = this.json_urban;
-            console.log(this.urban_display);
-          })
-          .catch(error => {
-            console.log(error);
-          })
+        this.search_words(e);
+        this.search_urban(e);
       }
+    },
+    search_words (e) {
+      const query = e.target.value;
+      const url = "https://wordsapiv1.p.rapidapi.com/words/" + query;
+      const headers = {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": config.WORD_KEY,
+      }
+      axios
+        .get(url, {headers})
+        .then(response => {
+          this.json_words = response.data.results;
+          this.words_display = this.json_words;
+          console.log(this.words_display);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    },
+    search_urban (e) {
+      const query = e.target.value;
+      const url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define";
+      const headers = {
+        "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com",
+        "x-rapidapi-key": config.URB_KEY,
+      }
+      const params = {
+        term: query
+      }
+      axios
+        .get(url, {params, headers})
+        .then(response => {
+          this.json_urban = response.data.list;
+          this.urban_display = this.json_urban;
+          // console.log(this.urban_display);
+        })
+        .catch(error => {
+          console.log(error);
+        })
     },
   }
 })
